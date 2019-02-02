d4653e5940081d6eb20ff13f572adde43d600370
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2019-02-02 17:52:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -285,8 +285,24 @@ client.on('ready', () => {
     client.user.setStatus("DND");
 });
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "#";
+client.on("message", message => {
 
- 
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : i1Suhaib`);
@@ -295,9 +311,8 @@ client.on('ready', () => {
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`Mafia`,"http://twitch.tv/S-F")
-client.user.setStatus("online")
+client.user.setStatus("dnd")
 });
-
 
 
 
